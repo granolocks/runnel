e61ac8df382cbe5baf47b51dd7d7dfa841d6976a
Gabe Koss <gabe@gabekoss.com>
2013-11-07 20:59:45 -0500

    initial reorganization into lib dir, completely untested! :)

---
+++ lib/runnel.rb
@@ -0,0 +1,55 @@
+require 'runnel/auto_ssh'
+
+module Runnel
+  RUNNEL_DIR    = ENV['HOME']+"/.runnel"
+  PIDS_DIR      = "#{RUNNEL_DIR}/pids"
+  PIDS_FILES    = "#{PIDS_DIR}/*"
+  TUNNEL_CONFIG = "#{RUNNEL_DIR}/tunnels.yml"
+
+  def self.config
+    @config = YAML.load_file(TUNNEL_CONFIG)
+  end
+
+  def self.setup
+    puts "mkdir -p #{RUNNEL_DIR} #{PIDS_DIR}"
+    `mkdir -p #{RUNNEL_DIR} #{PIDS_DIR}`
+    unless File.exists?(TUNNEL_CONFIG)
+      puts "Populating an example config"
+      File.open(TUNNEL_CONFIG, 'w').puts(TCONF_EXAMPLE)
+    end
+    puts "Now just update #{TUNNEL_CONFIG}"
+  end
+
+  def self.all
+    list = []
+    config.each_pair do |k,v|
+      list << AutoSsh.new(k, v)
+    end
+    list
+  end
+
+  def self.kill(tid)
+    create_from_tunnel_id(tid).kill
+  end
+
+  def self.start(tid)
+    tunnel = create_from_tunnel_id(tid)
+    tunnel.start
+  end
+
+  def self.start_all
+    self.all.each do |t|
+      t.start unless t.running?
+    end
+  end
+
+  def self.kill_all
+    self.all.each do |t|
+      t.kill if t.running?
+    end
+  end
+
+  def self.create_from_tunnel_id(tid)
+    AutoSsh.new(tid, config[tid.to_sym])
+  end
+end
---
+++ lib/runnel/auto_ssh.rb
@@ -0,0 +1,60 @@
+module Runnel
+  class AutoSsh
+    attr_reader :id, :config
+    alias_method :conf, :config
+
+    def initialize(id, config)
+      @id = id
+      @config = config
+    end
+
+    def pp_description
+      txt = "#{id} - #{conf[:name]}"
+      txt += " (Running: #{pid})" if running?
+      running? ? green(txt) : red(txt)
+    end
+
+    def running?
+      if File.exists?(pid_file)
+        if `ps #{pid} | grep autossh`.length == 0
+          File.delete(pid_file)
+          false
+        else
+          true
+        end
+      end
+    end
+
+    def start
+      puts "Starting #{conf[:name]}"
+      puts "AUTOSSH_PIDFILE=#{pid_file} autossh -M #{conf[:mport]} #{conf[:command]}"
+      ENV['AUTOSSH_PIDFILE'] = pid_file
+      `autossh -M #{conf[:mport]} #{conf[:command]}`
+    end
+
+    def kill
+      if running?
+        `kill #{pid}`
+      else
+        puts "Unable to find a running #{conf[:name]}"
+      end
+    end
+
+    private
+
+    def pid_file
+      "#{PIDS_DIR}/#{id}"
+    end
+
+    def pid
+      File.read(pid_file).chomp
+    end
+
+    def red(txt)
+      "\033[31m#{txt}\033[0m"
+    end
+
+    def green(txt)
+      "\033[32m#{txt}\033[0m"
+    end
+  end
